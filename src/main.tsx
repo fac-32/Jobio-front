48008d8f2861761c4fe04f946d36e397064c3f4c
Anson Chung <ansonchung3@gmail.com>
2025-12-03 10:08:25 +0000

    Correct the content inside BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import Home from './pages/home';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <Home />
+        <App />
     </BrowserRouter>,
 );
