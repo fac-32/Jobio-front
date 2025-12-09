a025f546a7d5a9767639f70ea4d5a7fe8aeb2714
Mark O Meara <markomeara98@gmail.com>
2025-12-09 11:49:28 +0000

    Added in page for after confirming email address

---
+++ src/pages/ConfirmEmail.tsx
@@ -0,0 +1,13 @@
+
+export default function ConfirmEmail() {
+    
+    return (
+        <div className="flex flex-col items-center mt-10 px-4">
+            <h1 className="text-3xl font-bold text-indigo-600 mb-6">
+                Thank you for confirming your email
+            </h1>
+
+            <p>Please return to the website and login in with your details.</p>
+        </div>
+    );
+}
