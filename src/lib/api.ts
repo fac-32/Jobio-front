8c1df35a6956852b01f3bdbb3b5fcdc4b052bf16
Anson Chung <ansonchung3@gmail.com>
2025-12-16 15:52:28 +0000

    Fix backed target for deployment

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://jobio-back.onrender.com';
 
 export async function api(path: string, options: RequestInit = {}) {
     // prepare headers
